b438cb4d7cb2dbb2c52e6a740ac780892fd74085
“WhyRoy” <740495357@qq.com>
2020-04-21 11:52:38 +0800

    优化代码

--- 杂七杂八的练习/Mosh-控制流练习/exercise11.js
+++ 杂七杂八的练习/Mosh-控制流练习/exercise11.js
@@ -6,7 +6,8 @@ function showPrimes(limit) {
         isPrime(i);
     //之所以能去掉大括号是因为for循环里只有一个语句
 }
-//判断是不是质数
+//判断是不是质数，这个方法不好，因为计算量太大了。
+/*
 function isPrime(num) {
     let counter =0;
     for (let j = 1; j <=num ; j++) {
@@ -14,4 +15,17 @@ function isPrime(num) {
         if (m ===0) counter++;
     }
     if (counter === 2) console.log(num);
+}*/
+//发现有其他数字是因子，就直接break，并打上不是质数的标签
+function isPrime(num) {
+    let isPrime = true;
+    for (let j = 2; j < num; j++) {
+        if (num % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime){
+     console.log(num);
+    }
 }
