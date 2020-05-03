6f3e3255900ae3929f7f15e680f9ea7606caf403
“WhyRoy” <740495357@qq.com>
2020-05-03 21:45:26 +0800

    集成练习

---
+++ Mosh-JsOPP练习/inheritanceChapter/exercise1.js
@@ -0,0 +1,29 @@
+//题目是一个特殊的要求,原型继承时有些特殊
+
+function HtmlEle() {
+    this.click = function () {
+        console.log('click');
+    }
+}
+HtmlEle.prototype.focus = function () {
+    console.log('focus');
+}
+const e = new HtmlEle();
+console.log(e);
+
+function SelectHtml(...args) {
+    this.items = [...args];
+    this.addItem = function (item) {
+        this.items.push(item);
+    }
+    this.removeItem = function (item) {
+        let temp = this.items.indexOf(item);
+        this.items.splice(temp,1);
+    }
+}
+SelectHtml.prototype = new HtmlEle();
+SelectHtml.prototype.constructor = SelectHtml;
+
+const  s = new SelectHtml(1,2,3);
+console.log(s);
+console.log(SelectHtml.prototype);
